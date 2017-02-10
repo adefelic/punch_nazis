3581803eb7a1132a25c93943988cc331ad4c9024
adefelic <ad.defelice@live.com>
2017-02-10 15:51:38 -0500

    moved actors into a class. they unfortunately do not share geometry

--- js/Actor.js
+++ js/Actor.js
@@ -1,58 +1,37 @@
-// adapted from http://www.96methods.com/2012/02/three-js-very-basic-animation/
-
-var Actor = function(mesh) {
+var Actor = function(mesh, render_order, scene) {		
 
 	this.mesh = mesh;
-	this.render_order;
+	this.mesh.renderOrder = render_order;
+	this.scene = scene;
+	this.lateralSpeed = 0;
 	
+	// set initial position
+	this.mesh.position.z = 0;
+	this.mesh.rotation.x = .15;
+	this.mesh.geometry.translate(0, 100.8, 0); // so we can rotate around the origin
+	this.mesh.geometry.rotateZ((Math.random() - 0.5) * .05); // rotate laterally
+
 	// set initial lateral movement speed
+	this.updateSpeed();
+
+	// add to scene
+	this.scene.add(this.mesh);
+}
+
+Actor.prototype.updateSpeed = function() {
 	var rand = Math.random() - 0.5;
-	if (Math.abs(rand) <= .06) rand = 0; 
-	this.speed = 0.003 * Math.pow(rand, 2);
-
-	this.in_scene = false;
-
-	this.animation_cycle_duration = 1000;
-	this.n_keyframes = 30;
-
-	this.current_keyframe;
-	this.last_keyframe; // previous?
-
-	this.last_frame_rendered_flag = false;
-
+	if (Math.abs(rand) <= .06) rand = 0;
+	this.lateralSpeed = 0.003 * Math.pow(rand, 2);
 }
 
-Actor.prototype.load = function() {
-	var loader = new THREE.TextureLoader();
-	var texture = loader.load(this.sprite);
-	Actor.prototype.setMesh(
-		new THREE.Mesh( new THREE.PlaneGeometry(1,2), new THREE.MeshLambertMaterial( { color: 0xFF6060, map: texture } ));
-	);
-	this.loaded_model = true;
+Actor.prototype.update = function() {
+	// rotate characters around the world
+	this.mesh.rotation.x += 0.0005;
+	// move character laterally
+	this.mesh.rotation.z += this.lateralSpeed;
 }
 
-Actor.prototype.hasLoaded = function() {
-	return this.loaded_model;
-}
-
-Actor.prototype.inScene = function() {
-	return this.in_scene;
-}
-
-Actor.prototype.addToScene = function(scene) {
-	scene.add(Actor.prototype.getMesh);
-	this.in_scene = true;
-}
-
-Actor.prototype.animate = function() {
-	// calculate how long to show a single frame
-	var interpolation = this.animation_cycle_duration / this.n_keyframes;
-
-	// determine current keyframe
-	var time = Date.now() % this.animation_cycle_duration;
-	var keyframe = Math.floor( time / interpolation ) + 1;
-	if (keyframe != current_keyframe) {
-		
-	}
+Actor.prototype.remove = function() {
+	this.scene.remove(this.mesh);
 }
 
--- js/game.js
+++ js/game.js
@@ -4,17 +4,16 @@
  * Date: February 2017
  */
 
-
 // some globals
-var container, scene, camera, renderer;
+var scene, camera, renderer;
 var clock = new THREE.Clock();
 
 // my globals below here
 var loader = new THREE.TextureLoader();
 var world_mesh;
 var actors = [];
-var actor_speeds = [];
 var render_order = 1000; // this is a hack
+var actor_material1, actor_material2;
 
 init();
 animate();
@@ -29,12 +28,10 @@ function init() {
 	camera.position.x = 0;
 	camera.position.y = 98.2;
 	camera.position.z = 25;
-	//scene.add(camera);
-	//camera.position.y = 50;
-	//camera.position.z = 200;
 
 	// Renderer
 	renderer = new THREE.WebGLRenderer();
+	//renderer.sortObjects = false;
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
 
@@ -48,9 +45,7 @@ function init() {
 	var radius = 100;
 	var world_geometry = new THREE.SphereGeometry(radius, 128, 128);
 	loader = new THREE.TextureLoader();
-	var world_tex = loader.load(
-		'textures/patchygrass_1.jpg'
-	);
+	var world_tex = loader.load('textures/patchygrass_1.jpg');
 	world_tex.wrapS = world_tex.wrapT = THREE.RepeatWrapping;
 	world_tex.repeat.set( 20, 20 );
 	var world_material = new THREE.MeshBasicMaterial( { color: 0x777733, map: world_tex } );
@@ -63,31 +58,27 @@ function init() {
 	var sky_material = new THREE.MeshBasicMaterial( {color: 0x7ec0ee} );
 	var sky_mesh = new THREE.Mesh( sky_geometry, sky_material );
 	sky_mesh.position.z = -50;
-	scene.add( sky_mesh );
-	scene.fog = new THREE.FogExp2( 0x9999ff, 0.00025 );
-}
+	scene.add(sky_mesh);
 
+	// Actor meshes
+	var actor_tex1 = loader.load('textures/nazi1a.png');
+	actor_material1 = new THREE.MeshBasicMaterial( { map: actor_tex1, transparent: true, depthWrite: false } );
+	var actor_tex2 = loader.load('textures/nazi2a.png'); 
+	actor_material2 = new THREE.MeshBasicMaterial( { map: actor_tex2, transparent: true, depthWrite: false } );
+}
 
 function newActor() {
-	var actor_geometry = new THREE.PlaneGeometry(0.8,1.6);
-	var actor_tex = loader.load('textures/nazi1a.png');
-	var actor_material = new THREE.MeshBasicMaterial( { map: actor_tex, transparent: true, depthWrite: false } );
-	var actor_mesh = new THREE.Mesh( actor_geometry, actor_material );
-	// actors.push(new Actor(nazi_mesh));
-	actor_mesh.renderOrder = render_order--;
+	var material;
+	// i should make different types behave differently
+	if (Math.random() < .5) {
+		// tall
+		material = actor_material1;
+	} else {
+		// short
+		material = actor_material2;
+	}
+	actors.push(new Actor(new THREE.Mesh(new THREE.PlaneGeometry(0.8, 1.6), material), render_order--, scene));
 	if (render_order == 0) render_order = 1000;
-	actor_mesh.position.z = 0;
-	actor_geometry.translate(0, 100.8, 0);
-	actor_geometry.rotateZ((Math.random() - 0.5) * .1);
-	actors.push( actor_mesh ); // our running collection of actors
-	scene.add( actor_mesh );
-	actor_speeds.push(newSpeed());
-}
-
-function newSpeed() {
-	var rand = Math.random() - 0.5;
-	if (Math.abs(rand) <= .06) rand = 0;
-	return 0.003 * Math.pow(rand, 2);
 }
 
 function animate() {
@@ -97,9 +88,42 @@ function animate() {
 }
 
 function update() {
-	var delta = clock.getDelta();
+	var time = Date.now();
+
+	// compare # of scene children w/ # of manually tracked actors
+	console.log(scene.children.length, actors.length);
+
+	// generate new Actors
+	if (time % 5 == 0) newActor();
+
+	// spin the world
+	world_mesh.rotation.x += 0.001;
+
+	// handle individual Actors
+	for (var i = actors.length - 1; i >= 0; i--) {
+
+		actors[i].update();
+
+		// pseudorandomly change character lateral speed
+		if (time % 10+i == 0) {
+			actors[i].updateSpeed();
+		}
+
+		// if character hits a side boundary, reverse direction
+		if (Math.abs(actors[i].mesh.rotation.z) > .01) {
+			actors[i].lateralSpeed *= -1;
+		}
+
+		// if the character has moved past the near plane, deallocate them
+		if (actors[i].mesh.rotation.x >= .3) {
+			actors[i].remove();
+			delete actors[i];
+			actors.splice(i, 1);
+		}
+	}
 }
 
+// stole this from an example
 function onWindowResize() {
 	windowHalfX = window.innerWidth / 2;
 	windowHalfY = window.innerHeight / 2;
@@ -109,40 +133,6 @@ function onWindowResize() {
 }
 
 function render() {
-	var time = Date.now();
-
-	// handle characters
-	for (var i = actors.length - 1; i >= 0; i--) {
-		// pseudorandomly change character lateral motion
-		if (time % 10+i == 0) {
-			actor_speeds.shift();
-			actor_speeds.push(newSpeed());
-		}
-		// if character hits a side boundary, reverse direction
-		if ( Math.abs(actors[i].rotation.z) > .015 ) {
-			actor_speeds[i] *= -1;
-		}
-		// move character laterally
-		actors[i].rotation.z += actor_speeds[i];
-
-		// if the character has moved past the near plane, deallocate them
-		if (actors[i].rotation.x >= .3) {
-			actors[i].material.dispose();
-			actors[i].geometry.dispose();
-			scene.remove(actors[i]);
-		}
-
-		// rotate characters around the world
-		actors[i].rotation.x += 0.0005;
-	}
-	// generate new characters
-	if (time % 5 == 0) newActor();
-
-	// spin the world
-	world_mesh.rotation.x += 0.001;
-	
 	renderer.render(scene, camera);
 }
-
-animate();
 
